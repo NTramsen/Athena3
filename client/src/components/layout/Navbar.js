6637830d40e53a5b9800ca17d44c58048fb4f2e6
Anthony <anthonywongfl99@gmail.com>
2020-10-26 09:36:46 -0400

    changed banner to Athena

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -14,6 +14,8 @@ class Navbar extends Component {
               }}
               className="col s5 brand-logo center black-text"
             >
+              <i className="material-icons">code</i>
+              Athena
             </Link>
           </div>
         </nav>
