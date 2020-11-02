61bcd3760853bf6038b2fb2ee9a0475680922b3f
zhouxin0918 <zhouxin.zuo@emory.edu>
2020-11-02 10:32:32 -0500

    Misc UI

--- client/src/components/AdminLandingPage/AdminDashboard/AdminDashboard.js
+++ client/src/components/AdminLandingPage/AdminDashboard/AdminDashboard.js
@@ -3,8 +3,10 @@ import {Link} from 'react-router-dom';
 import * as FaIcons from 'react-icons/fa';
 import './AdminDashboard.css';
 
+
 const AdminDashboard = ()=>{
 
+	
 	const getItems = ()=>{
 		return [
 			{
@@ -14,12 +16,12 @@ const AdminDashboard = ()=>{
 			},
 			{
 				name: "Behavioural biology textbook",
-				return_date: "2 days",
+				return_date: "2 days ",
 				item_id: 48920
 			},
 			{
 				name: "Design kit",
-				return_date: "6 days",
+				return_date: "6 days ",
 				item_id: 98384
 			}
 		];
@@ -28,18 +30,31 @@ const AdminDashboard = ()=>{
 	return(
 		<div className='dashboard-container'>
 			<div className='dashboard-header'>
-				<p>Your items</p>
+				<div className='dashboard-title'>
+					Your items
+				</div>
 			</div>
+			<div className='list_header'>
+				<div className='item_header'>
+					<span className="header_style1">Item Name</span>
+					<span className="header_style2">Return Date</span>
+					<span className="header_style3">Item ID</span>
+				</div>
+			</div>
+			
 			<div className='dashboard-list'>
 				<ul className='dashboard-list-items'>
+					
 					{getItems().map((item, index)=>{
 						return(
 							<li key={index} className="itemlist-element">
-								<Link to = "./item">
-									<span>{item.name}</span>
-									<span>{item.return_date}</span>
-									<FaIcons.FaArrowAltCircleRight size={20}/>
+								<Link to = "./item" className="item_a">
+										<span className="item_name">{item.name}</span>
+										<span className="item_date">{item.return_date}</span>
+										<span className="item_id">{item.item_id}</span>
+										
 								</Link>
+
 							</li>
 						)
 					})}
